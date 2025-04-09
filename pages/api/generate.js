2885bd3181dbee0d27a953e9d1f4e9aa39522027
radwizindustries <radwizinc@gmail.com>
2025-04-09 23:21:57 +0000

    "Generate"

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -21,7 +21,8 @@ export default async function handler(req, res) {
 
   try {
     const output = await replicate.run(
-      "stability-ai/stable-diffusion",
+     "stability-ai/stable-diffusion:db21e45e2c50842f8d7f3581b9c2a44fae9053b4",
+
       {
         input: { prompt },
       }

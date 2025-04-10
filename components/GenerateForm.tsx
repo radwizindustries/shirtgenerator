445ff1404b3c4865ededd5972869a3926836bd43
radwizindustries <radwizinc@gmail.com>
2025-04-10 05:55:09 +0000

    Fix: use relative import for generateImage to resolve module error

--- components/GenerateForm.tsx
+++ components/GenerateForm.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react"
-import { generateImage } from "@/lib/generateImage";
+import { generateImage } from "../lib/generateImage";
 import { saveGeneratedShirt } from "../lib/saveGeneratedShirt"
 
 export default function GenerateForm() {

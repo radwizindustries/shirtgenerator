8b8956edc636d473c81fce9b9dcd81a4dceb7d95
radwizindustries <radwizinc@gmail.com>
2025-04-11 20:24:58 -0700

    Update login and signup pages to use next/navigation

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { useAuth } from '../components/AuthProvider';
 import Link from 'next/link';
 

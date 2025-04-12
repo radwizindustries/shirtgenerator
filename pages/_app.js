20b98580fe95873d56907a42123f302985b3a5c9
radwizindustries <radwizinc@gmail.com>
2025-04-11 20:40:44 -0700

    Add favicon and update app head

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
 import { AuthProvider } from '../components/AuthProvider'
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Shirt Generator</title>
+      </Head>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </>
   )
 }
 
